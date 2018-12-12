ad6da9b09fc4b2868bfcddab2c67149046dd0b5a
Cy Ellison <dragonz2626@gmail.com>
2018-12-12 13:50:49 -0700

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Ellison, Cy on 12/12/18.
+//  Copyright © 2018 Ellison, Cy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
